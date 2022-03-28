0a3518330ce98f60486e75b1226c4a45c8c6a919
KhinNyeinChan <poephayound18@gmail.com>
2022-03-28 22:16:41 +0630

    update intro and add about component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
 
   
@@ -5,6 +6,7 @@ const App = () => {
     return (
       <div>
         <Intro />
+        <About />
       </div>
     );
   };
